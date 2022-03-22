3cbc916b31658ba98106aa93351209019ca59cb2
hantsy <hantsy@gmail.com>
2022-03-22 22:39:06 +0800

    chore: clean codes

--- data-r2dbc/src/main/java/com/example/CustomerRepository.java
+++ data-r2dbc/src/main/java/com/example/CustomerRepository.java
@@ -1,13 +1,11 @@
 package com.example;
 
-import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.r2dbc.annotation.R2dbcRepository;
 import io.micronaut.data.repository.reactive.ReactorCrudRepository;
 
 import java.util.UUID;
 
-@Repository()
 @R2dbcRepository(dialect = Dialect.POSTGRES)
 public abstract class CustomerRepository implements ReactorCrudRepository<Customer, UUID> {
 
