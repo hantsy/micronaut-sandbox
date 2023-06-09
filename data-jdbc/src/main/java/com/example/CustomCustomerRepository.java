e27148fa0e05c4ca010d6dbaa368250f830a2199
hantsy <hantsy@gmail.com>
2023-06-09 15:20:59 +0800

    chore: clean codes

--- data-jdbc/src/main/java/com/example/CustomCustomerRepository.java
+++ data-jdbc/src/main/java/com/example/CustomCustomerRepository.java
@@ -1,15 +1,12 @@
 package com.example;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CustomCustomerRepository {
-    @Transactional
     List<Customer> findAll();
 
-    @Transactional
     Optional<Customer> findById(UUID id);
 
     UUID save(Customer data);
